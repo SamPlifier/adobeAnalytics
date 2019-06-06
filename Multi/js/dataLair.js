336274d67743d6f46e8d72b4290cf5f96c1fbfec
Sam Davis-Castro <sam.davis-castro@ICIDigitals-MacBook-Pro-199.local>
2019-06-06 13:28:04 -0400

    added basic data layer

---
+++ Multi/js/dataLair.js
@@ -0,0 +1,13 @@
+// dataLair = dataLayer #worstEasterEgg
+$(document).ready(function() {
+  var pageName = $('.nav-tabs .active')[0].innerHTML;
+  var digitalData = {
+    page: {
+      pageName: pageName,
+      siteSection: "we only have 1"
+    },
+    customer: {
+      loginStatus: "Logged Out"
+    }
+  }
+});
